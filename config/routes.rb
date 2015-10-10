fcc18c0ff06fab99d480f4ba27de58ee19bc2363
revivre <lilli.life@gmail.com>
2015-10-11 01:16:08 +0900

    layouts and sessions modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root                'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   get 'users/new'
   get 'signup'  => 'users#new'
   resources :users
