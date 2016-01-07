65feb81662f35cf2cb1af9d3490be231ee591d2b
revivre <lilli.life@gmail.com>
2016-01-08 02:34:19 +0900

    change weekday locale to ja

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
   def format_date(date)
     return nil unless date
-    date.strftime('%m/%d(%a)')
+    date.strftime('%m/%d') + '(' + day_letter(date.wday) + ')'
   end
+
   def format_time(date)
     return nil unless date
     date.strftime('%H:%M')
+  end
+
+  def day_letter(day)
+    I18n.t('date.abbr_day_names')[day % 7].first
   end
 end
