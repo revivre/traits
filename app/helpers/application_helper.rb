45709d5c8b480bfaea20baea4d5f434201e143ef
revivre <lilli.life@gmail.com>
2016-01-08 02:20:09 +0900

    add format_time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   def format_date(date)
     return nil unless date
+    date.strftime('%m/%d(%a)')
+  end
+  def format_time(date)
+    return nil unless date
     date.strftime('%H:%M')
   end
 end
