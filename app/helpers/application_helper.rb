718c7e923f6e0ac715baefc525fdb387dd118044
revivre <lilli.life@gmail.com>
2016-01-12 01:12:07 +0900

    app helper method move to work times helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,2 @@
 module ApplicationHelper
-  def format_date(date)
-    return nil unless date
-    date.strftime('%m/%d') + '(' + day_letter(date.wday) + ')'
-  end
-
-  def format_time(date)
-    return nil unless date
-    date.strftime('%H:%M')
-  end
-
-  def day_letter(day)
-    I18n.t('date.abbr_day_names')[day % 7].first
-  end
-
-  def check_holiday(work_date, first_day, last_day)
-    if (HolidayJp.between(first_day, last_day).has_key?(work_date))
-      return true
-    else
-      return false
-    end
-  end
 end
