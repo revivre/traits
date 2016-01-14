fc0217753ca189ab34b2951fd504ff6189a88b5c
revivre <lilli.life@gmail.com>
2016-01-15 00:34:07 +0900

    add validates of length for WorkTime.note

--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -1,5 +1,6 @@
 class WorkTime < ActiveRecord::Base
   belongs_to :user
+  validates :note, length: { maximum: 100 }
 
   enum holiday_enum: { "" => 0, "欠勤" => 1, "慶弔" => 2, "有給" => 3, "代休" => 4, "その他" => 5 }
 end
