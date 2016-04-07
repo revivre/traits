1764fe665f86328179d2791334c96f29332080a6
revivre <lilli.life@gmail.com>
2016-04-07 23:54:32 +0900

    delete user_pages_spec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "UserPages", type: :request do
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
 
-    it { expect(page).to have_content(user.name) }
-  end
 end
